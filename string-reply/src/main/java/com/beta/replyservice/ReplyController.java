961268ef10034ea8c1f04e60acbbd4822d8e9b2c
fuadazhar73 <fuadazhar73@gmail.com>
2021-09-15 20:11:31 +0700

    update

--- string-reply/src/main/java/com/beta/replyservice/ReplyController.java
+++ string-reply/src/main/java/com/beta/replyservice/ReplyController.java
@@ -40,6 +40,47 @@ public class ReplyController {
 			String reverse =input1.reverse().toString();
 			String md5Hex = DigestUtils.md5Hex(reverse);
 			return new ResponseEntity<>(new ReplyMessage(md5Hex),HttpStatus.OK);
+		}else if(cek1.equals("21")){
+			String md5Hex = DigestUtils.md5Hex(cek2);
+			input1.append(md5Hex);
+			String reverse =input1.reverse().toString();
+			return new ResponseEntity<>(new ReplyMessage(reverse),HttpStatus.OK);
+		}else if(cek1.equals("22")){
+			String md5Hex = DigestUtils.md5Hex(cek2);
+			String md5Hex2x = DigestUtils.md5Hex(md5Hex);
+			return new ResponseEntity<>(new ReplyMessage(md5Hex2x),HttpStatus.OK);
+		}
+	}else{
+			return new ResponseEntity<>(new ReplyMessage(message),HttpStatus.OK);
+	}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ReplyMessage("invalid Input"));
+	}
+	@GetMapping("/v2/reply/{message}")
+	@ResponseBody
+	public ResponseEntity<Object> replyingv2(@PathVariable String message) {
+		StringBuilder input1 = new StringBuilder();
+		if(message.contains("-")){
+		String[] messageData = message.split("-");
+		String cek1 = messageData[0];
+		String cek2 = messageData[1];
+		if(cek1.equals("1")){
+			input1.append(cek2);
+			return new ResponseEntity<>(new ReplyMessage(input1.reverse().toString()),HttpStatus.OK);
+		}else if(cek1.equals("2")){
+			    String md5Hex = DigestUtils.md5Hex(cek2);
+			return new ResponseEntity<>(new ReplyMessage(md5Hex),HttpStatus.OK);
+		}else if(cek1.equals("11")){
+			return new ResponseEntity<>(new ReplyMessage(cek1),HttpStatus.OK);
+		}else if(cek1.equals("12")){
+			input1.append(cek2);
+			String reverse =input1.reverse().toString();
+			String md5Hex = DigestUtils.md5Hex(reverse);
+			return new ResponseEntity<>(new ReplyMessage(md5Hex),HttpStatus.OK);
+		}else if(cek1.equals("21")){
+			String md5Hex = DigestUtils.md5Hex(cek2);
+			input1.append(md5Hex);
+			String reverse =input1.reverse().toString();
+			return new ResponseEntity<>(new ReplyMessage(reverse),HttpStatus.OK);
 		}else if(cek1.equals("22")){
 			String md5Hex = DigestUtils.md5Hex(cek2);
 			String md5Hex2x = DigestUtils.md5Hex(md5Hex);
